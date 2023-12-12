52e2dbaced7505899b09b1521f264b8e2c075df9
Kristian Lewis <kristianedwardlewis@gmail.com>
2023-12-12 16:45:57 -0500

    Add more info data

--- src/metaData.js
+++ src/metaData.js
@@ -6,13 +6,19 @@ const metaData = [
     },
     {
         name : "File Manager",
-        info : ["This is a file manager"],
+        info : ["This program allows you to access and manage the filesystem.\n\nClick on a folder to view its contents.\n\nClick on a program to start the program.\n\nClick on a file to open that file in its associated program.",
+                "Right clicking opens a menu of options depending on what you click on.\n\nOn touch devices you can right click by pressing down for a few seconds.",
+                "Right click in the dark region to add new folders and files.\n\nImages and pdf files will have to be \"downloaded\" from your computer.\n\n(These files are not sent over the internet)",
+                "Right click on a file to delete it.\n\nRight click on a folder to delete it, or add it to the quick access menu on the left.\n\nRight click on a folder in the quick access menu to remove it from quick access.\n\nThe default folders and files can not be deleted.",
+                "You can click on a folder name in the path to traverse to that folder.\n\nIf the current path becomes too large for its container, you can scroll though it."],
         defaultDimensions : {defaultWidth: 720, defaultHeight : 500},
     },
     {
         name : "PianoSynthJS",
-        info : ["This is a simple Piano Synthesizer"],
-        defaultDimensions : {defaultWidth: 370, defaultHeight : 630},
+        info : ["This is a simple Piano Synthesizer.\n\nBefore you can start playing the piano, you need at least one \"Sound\". Click on \"Add New Sound\".",
+                "After clicking on \"Add New Sound\" you should see a box with the words \"Sine Wave\".\n\nClicking on this will open the sound's settings. From here you can change the Octave, Volume, and Wave Type of that sound.",
+                "Clicking play will play the sound continuously for testing purposes.\n\nTo play a single note simply click on a key on the piano.\n\nYou can also click on the \"Click to use keyboard\" section to use your keyboard."],
+        defaultDimensions : {defaultWidth: 370, defaultHeight : 700},
     },
     {
         name : "Calculator",
@@ -21,20 +27,23 @@ const metaData = [
     },
     {
         name : "Image Viewer",
-        info : ["You can zoom in or out by scrolling the mouse wheel over the image.\nClicking on the picture will zoom in and holding down CTRL while clicking will zoom out.",
-            "You also have the option to select a zoom level from the Files menu.",
+        info : ["You can zoom in or out by scrolling the mouse wheel over the image.\n\nClick on the picture to zoom in. Hold down CTRL and click to zoom out.\n\nYou can also select a zoom level from the Files menu.",
             "You can load a new image from the files menu. This will open a file manager to choose an image."],
         defaultDimensions : {defaultWidth: 720, defaultHeight : 500}
     },
     {
+        //This should be renamed to pdf reader after filemanager is refactored
         name : "PDF Viewer",
-        info : ["Currently this uses your browsers default pdf reader. This has mixed results depending on your browser. Mobile users will likely have issues using this program.",
-                "You can load a new PDF from the files menu. This will open a file manager to choose a PDF."],
+        info : ["Currently this uses your browser's built in PDF reader.\n\nThis has mixed results depending on your browser.\n\nMobile users will likely have issues using this program.",
+                "You can load a new PDF from the Files menu.\n\nThis will open a file manager to choose a PDF file."],
         defaultDimensions : {defaultWidth: 720, defaultHeight : 800}
     },
     {
         name : "Text Editor",
-        info : ["This is a text editor. "],
+        info : ["This program allows you to create, load, and save text documents.\n\nThe Files menu has three options. New File, Save File and Load File.",
+                "Clicking on \"New File\" will wipe the text document and remove its association with a file.\n\nYou will lose any unsaved progress on your current document.",
+                "Clicking on \"Save File\" will save the currently loaded file.\n\nIf no file has been loaded yet, it will open a file manager to select a file to save to.\n\nRight click in file manager to create a new text document.",
+                "Clicking on \"Load File\" will open a file manager to select a file to load."],
         defaultDimensions : {defaultWidth: 720, defaultHeight : 600}
     },
 ]
